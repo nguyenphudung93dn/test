2950b4b424da3dbcdc011a5d303f2cdadf60ae03
Nguyen Phu Dung <nguyenphudung93.dn@gmail.com>
2018-09-24 10:18:00 +0700

    create route laravel

--- Blog_laravel/routes/web.php
+++ Blog_laravel/routes/web.php
@@ -14,3 +14,49 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test_route',function(){
+    return 'Hello world';
+});
+
+Route::get('tester/route_2',function(){
+    echo "<h1>Laravel test route </h1>";
+});
+//truyen tham so
+Route::get('HoTen/{ten}',function($ten){
+    echo "Ten cua ban La: ".$ten;
+});
+
+Route::get('Laravel/{ngay}',function($ngay){
+    echo "Khoa Pham: " .$ngay;
+})->where(['ngay'=>'[0-9]+']);
+
+
+//dinh danh
+Route::get('Route1',['as'=>'MyRoute',function(){
+    echo "hello your " ;
+}]);
+
+Route::get('goiten',function(){
+    return redirect()->route('MyRoute');
+});
+//dinh danh c2
+Route::get('Route2',function(){
+    echo "đây là route 2 " ;
+})->name('MyRoute2'); 
+
+Route::get('goiten2',function(){
+    return redirect()->route('MyRoute2');
+});
+//tao nhom dinh danh
+Route::group(['prefix'=>'MyGroup'],function(){
+    Route::get('User1',function(){
+        echo 'USER1';
+    });
+    Route::get('User2',function(){
+        echo 'USER2';
+    });
+    Route::get('User3',function(){
+        echo "User3";
+    });
+});
